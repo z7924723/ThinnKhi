73032e3d681b3f792b4344aa8ea817913ded1037
PinguMac <>
2018-06-28 14:53:41 +0800

    Enhance DayMainInfoViewViewModel properties

--- ThinnKhi/View Controllers/Weather View Controllers/View Model/DayMainInfoViewViewModel.swift
+++ ThinnKhi/View Controllers/Weather View Controllers/View Model/DayMainInfoViewViewModel.swift
@@ -18,30 +18,37 @@ struct DayMainInfoViewViewModel {
   // MARK: -
   private let dateFormatter = DateFormatter()
   
-  var temperature: String {
-    let temperature = weatherData.currently.temperature
+  var currentTemperature: String {
+    let currentTemperature = weatherData.currently.temperature
+
+    return temperatureConversion(temperature: currentTemperature)
+  }
+  
+  var temperatureHigh: String {
+    let temperatureHigh = weatherData.daily.data[0].temperatureHigh
     
-    switch UserDefaults.temperatureNotation() {
-    case .fahrenheit:
-      return String(format: "%.1f °F", temperature)
-    default:
-      return String(format: "%.1f °C", temperature.toCelcius())
-    }
+    return temperatureConversion(temperature: temperatureHigh)
+  }
+  
+  var temperatureLow: String {
+    let temperatureLow = weatherData.daily.data[0].temperatureLow
+    
+    return temperatureConversion(temperature: temperatureLow)
   }
   
   var precipIntensity: String {
     let precipIntensity = weatherData.currently.precipIntensity
-    return String(format: "%f %", precipIntensity)
+    return String(format: "%.f %%", precipIntensity * 100)
   }
   
   var humidity: String {
     let humidity = weatherData.currently.humidity
-    return String(format: "%f %", humidity)
+    return String(format: "%.f %%", humidity * 100)
   }
   
   var windSpeed: String {
     let windSpeed = weatherData.currently.windSpeed
-    
+
     switch UserDefaults.unitsNotation() {
     case .imperial:
       return String(format: "%.f MPH", windSpeed)
@@ -50,7 +57,7 @@ struct DayMainInfoViewViewModel {
     }
   }
 
-  var image: String {
+  var weatherIcon: String {
     return String(weatherData.currently.icon)
   }
 
@@ -64,4 +71,13 @@ struct DayMainInfoViewViewModel {
     return String(city)
   }
 
+  // MARK: - Helper Method
+  private func temperatureConversion(temperature: Double) -> String {
+    switch UserDefaults.temperatureNotation() {
+    case .fahrenheit:
+      return String(format: "%.1f °F", temperature)
+    default:
+      return String(format: "%.1f °C", temperature.toCelcius())
+    }
+  }
 }

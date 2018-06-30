fdffe56e88669eb09fd256102e98d6600989cd85
PinguMac <>
2018-07-01 00:43:34 +0800

    Add SevenDaysInfoViewController.swift to config the weather of seven days

---
+++ ThinnKhi/View Controllers/Weather View Controllers/SevenDaysInfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  ThinnKhi
+//
+//  Created by PinguMac on 2018/7/1.
+//  Copyright © 2018年 PinguMac. All rights reserved.
+//
+
+import UIKit
+
+class SevenDaysInfoViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}

9d09f8516b2152cce7c1d5b25b0089af10319693
PinguMac <>
2018-07-01 00:38:31 +0800

    Add HomeTabBarController.swift to manager weather data

---
+++ ThinnKhi/View Controllers/Tab Bar Controller/HomeTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeTabBarController.swift
+//  ThinnKhi
+//
+//  Created by PinguMac on 2018/7/1.
+//  Copyright © 2018年 PinguMac. All rights reserved.
+//
+
+import UIKit
+
+class HomeTabBarController: UITabBarController {
+  
+  // Mark: - View Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}

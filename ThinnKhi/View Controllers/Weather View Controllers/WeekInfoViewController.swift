357d256831ac67fda6ce1a020959b05c807f6f89
PinguMac <>
2018-07-03 15:31:29 +0800

    Add notification observer in WeekInfoViewController

--- ThinnKhi/View Controllers/Weather View Controllers/WeekInfoViewController.swift
+++ ThinnKhi/View Controllers/Weather View Controllers/WeekInfoViewController.swift
@@ -7,7 +7,33 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeekInfoViewController: UIViewController {
   
+  // MARK: - Porperties
+  private var placemark: CLPlacemark?
+  
+  // MARK: - Initializers
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(showPlacemark),
+                                           name: .placemarkDidSet,
+                                           object: placemark)
+  }
+
+  // Mark: - View Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+  
+  // MARK: - Helper
+  @objc func showPlacemark(notification: NSNotification) {
+    placemark = notification.object as? CLPlacemark
+    if let placemark = placemark {
+      print(placemark.locality)
+    }
+  }
 }

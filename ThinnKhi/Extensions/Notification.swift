be0d3bfa2204e501594d9fbc42b20a6c705e18a2
PinguMac <>
2018-07-03 11:52:55 +0800

    Add extension to do data observer

---
+++ ThinnKhi/Extensions/Notification.swift
@@ -0,0 +1,14 @@
+//
+//  Notification.swift
+//  ThinnKhi
+//
+//  Created by PinguMac on 2018/7/3.
+//  Copyright © 2018年 PinguMac. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+  static let weatherDataDidSet = Notification.Name("WeatherDataDidSet")
+  static let currentLocationDidSet = Notification.Name("CurrentLocationDidSet")
+}

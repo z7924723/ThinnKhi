dbf58f735faf005af6b94479e5a8049047a918ea
PinguMac <>
2018-07-04 15:33:15 +0800

    Change background gradient color

--- ThinnKhi/Extensions/CAGradientLayer.swift
+++ ThinnKhi/Extensions/CAGradientLayer.swift
@@ -12,7 +12,7 @@ extension CAGradientLayer {
   
   func thinnKhiBackgroundColor() -> CAGradientLayer {
     let gradientLayer = CAGradientLayer()
-    let gradientColors = [#colorLiteral(red: 0.2431372549, green: 0.7882352941, blue: 0.7490196078, alpha: 1), #colorLiteral(red: 0, green: 0.5490196078, blue: 0.6431372549, alpha: 1), #colorLiteral(red: 0, green: 0.4470588235, blue: 0.5450980392, alpha: 1)]
+    let gradientColors = [#colorLiteral(red: 0, green: 0.4470588235, blue: 0.5450980392, alpha: 1), #colorLiteral(red: 0, green: 0.5490196078, blue: 0.6431372549, alpha: 1), #colorLiteral(red: 0.2431372549, green: 0.7882352941, blue: 0.7490196078, alpha: 1)]
     
     gradientLayer.colors = gradientColors.map { $0.cgColor }
     

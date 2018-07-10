3efaf9434945e396ca10cb5a7f0d8f0de005b635
PinguMac <>
2018-07-10 15:17:55 +0800

    Add weekInfoViewController for 7 days information

---
+++ ThinnKhi/View Controllers/Week Info View Controller/WeekInfoViewController.swift
@@ -0,0 +1,175 @@
+//
+//  WeekViewController.swift
+//  ThinnKhi
+//
+//  Created by PinguMac on 2018/6/25.
+//  Copyright © 2018年 PinguMac. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class WeekInfoViewController: UIViewController {
+  
+  // MARK: - Interface Builder Properties
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var districtLabel: UILabel!
+  
+  // MARK: - Porperties
+  private var placemark: CLPlacemark?
+  private var weekInfo: Daily?
+  private var weatherDayData: [DayData]?
+  
+  // MARK: -
+  private var viewModel: [DayDataViewModel]?
+
+  
+  // MARK: - Initializers
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+
+    configureObserver()
+  }
+
+  // Mark: - View Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    registerTableViewCell()
+
+    configViewModel()
+    
+    districtLabel.text = placemark?.locality
+    
+    let backgroundLayer = CAGradientLayer().thinnKhiBackgroundColor()
+    backgroundLayer.frame = self.view.bounds
+    self.view.layer.insertSublayer(backgroundLayer, at: 0)
+    
+  }
+  
+  // MARK: - Helper
+  private func configViewModel() {
+    viewModel = [DayDataViewModel]()
+
+    // get seven days of data
+    for index in 1..<(weatherDayData?.count)! {
+      viewModel?.append(DayDataViewModel(dayData: weatherDayData![index]))
+    }
+    
+    viewModel?.first?.isCollapsed = false
+  }
+  
+  private func registerTableViewCell() {
+    let headerNib = UINib.init(nibName: "WeekInfoTableViewSectionHeader", bundle: Bundle.main)
+    tableView.register(headerNib, forHeaderFooterViewReuseIdentifier: "WeekInfoTableViewSectionHeader")
+    
+    tableView?.register(WeekInfoTableViewCell.nib, forCellReuseIdentifier: WeekInfoTableViewCell.identifier)
+  }
+  
+  private func configureObserver() {
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(getPlacemark),
+                                           name: .placemarkDidSet,
+                                           object: placemark)
+    
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(getWeatherData),
+                                           name: .weatherDataDidSet,
+                                           object: weekInfo)
+  }
+  
+  @objc func getPlacemark(notification: NSNotification) {
+    placemark = notification.object as? CLPlacemark
+  }
+  
+  @objc func getWeatherData (notification: NSNotification) {
+    let dailyInfo = notification.object as? Daily
+    
+    weatherDayData = [DayData]()
+    for index in 0..<(dailyInfo?.data.count)! {
+      weatherDayData?.append((dailyInfo?.data[index])!)
+    }
+  }
+}
+
+extension WeekInfoViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "WeekInfoTableViewSectionHeader") as! WeekInfoTableViewSectionHeader
+
+    headerView.delegate = self
+    headerView.section = section
+
+    if let viewModel = viewModel?[section] {
+      headerView.configure(withViewModel: viewModel)
+    }
+    
+    return headerView
+  }
+
+  func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return 80
+  }
+  
+  func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+    return 0.0001
+  }
+  
+}
+
+extension WeekInfoViewController: UITableViewDataSource {
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    guard let viewModel = viewModel else {
+      return 0
+    }
+
+    return viewModel.count
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    guard let viewModel = viewModel else {
+      return 0
+    }
+
+    let cellData = viewModel[section]
+    
+    return cellData.isCollapsed ? 0 : 1
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: WeekInfoTableViewCell.identifier, for: indexPath) as? WeekInfoTableViewCell else {
+      fatalError("Unexpected Table View Cell")
+    }
+    
+    if let viewModel = viewModel?[indexPath.section] {
+      cell.configure(withViewModel: viewModel)
+    }
+    
+    return cell
+  }
+}
+
+extension WeekInfoViewController: HeaderViewDelegate {
+  
+  func toggleSection(header: WeekInfoTableViewSectionHeader, section: Int) {
+
+    if let headerData = viewModel?[section] {
+      if headerData.isCollapsible {
+        
+        // Toggle collapse
+        let collapsed = !(headerData.isCollapsed)
+        headerData.isCollapsed = collapsed
+        
+        self.tableView.beginUpdates()
+        self.tableView.reloadSections([section], with: .fade)
+        self.tableView.endUpdates()
+        
+        if headerData.isCollapsed == false {
+          let indexPath = IndexPath(row: tableView.numberOfRows(inSection: section) - 1, section: section)
+          self.tableView.scrollToRow(at: indexPath , at: .top, animated: true)
+        }
+      }
+    }
+  }
+  
+}
